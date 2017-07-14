e6f1b94c8ea32e610d669e892370fee959373e9d
HassanQbatch <hassan.tariq@qbatch.com>
2017-07-14 16:02:42 +0500

    GeoPatternAdded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	
+	protect_from_forgery with: :exception
+
+
+	before_action :geo_pattern
+	def geo_pattern
+		@pattern = GeoPattern.generate('Mastering Markdown', patterns: :plaid, base_color: '#FFFFFF')
+		@pattern.to_svg
+	end
+
 end
+#color: '#0154A6'
