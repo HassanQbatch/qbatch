56a9b5d6809cae5f81d909e3a7834d99fd942dbf
HassanQbatch <hassan.tariq@qbatch.com>
2017-07-17 11:04:42 +0500

    GoogleAdded

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,5 @@
 module WelcomeHelper
+	def google_map(center)
+		"https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=1000x300&zoom=15&markers=color:red%7Clabel:C%7C31.421714,73.063210"
+	end
 end
