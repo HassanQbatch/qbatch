2b2bc427adf6f1d2c944a50006b6e4afb59fe0e6
HassanQbatch <hassan.tariq@qbatch.com>
2017-07-20 18:14:09 +0500

    Messages Table Added

--- db/migrate/20170720123602_create_messages.rb
+++ db/migrate/20170720123602_create_messages.rb
@@ -1,6 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
+      t.string	:name
+      t.text	:message
+      t.string	:email
+      t.date	:date
 
       t.timestamps
     end
